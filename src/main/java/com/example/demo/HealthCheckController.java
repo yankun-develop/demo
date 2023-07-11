db82719f7a9cb37d663f0055f70608c798f7608a
yanku <yankun-develop@outlook.com>
2023-07-11 21:28:36 +0800

    init

--- src/main/java/com/example/demo/HealthCheckController.java
+++ src/main/java/com/example/demo/HealthCheckController.java
@@ -1,16 +1,14 @@
-package com.yuyuka.trading.center.service;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/")
-@RequiredArgsConstructor
 public class HealthCheckController {
-	@GetMapping("/healthcheck")
+	@GetMapping("/ok")
 	public String healthCheck() {
 		return "ok";
 	}
